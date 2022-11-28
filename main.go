f19aa505e26a325744cd813ca86efec683bd7f80
BuddhiLW <pedrogbranquinho@gmail.com>
2022-11-28 19:00:58 -0300

    Search files functionallity completed

--- main.go
+++ main.go
@@ -16,30 +16,31 @@ import (
 func main() {
 	var kegPath string
 	var inputRegex string
+	var caseSensitive string
 
 	homedir, err := os.UserHomeDir()
 	if err != nil {
-		log.Fatal("Couldn't decifer home directory for user")
+		log.Println(err, "Some error due to os.UserHomeDir; Couldn't decifer home directory for user")
+		log.Fatal(err)
 	}
 
 	flag.StringVar(&kegPath, "p", homedir+"/keg/", "path: the KEG path on the machine.")
 	flag.StringVar(&inputRegex, "reg", "reg", "regex: regular expression to be matched in keg nodes.")
+	flag.StringVar(&caseSensitive, "c", "", "case: default is insensitive; any string will make it case-sensitive")
 
 	flag.Parse()
 
-	// fmt.Println(kegPath, inputRegex)
-
 	files, err := ioutil.ReadDir(kegPath)
 	if err != nil {
-		log.Print(err, "Some error occured")
+		log.Print(err, "Some error occured, while reading kegPath")
+		log.Fatal(err)
 	}
 
 	for _, file := range files {
 		if _, err := strconv.Atoi(file.Name()); err == nil {
 			if file.IsDir() {
 				nodeName := kegPath + "/" + file.Name()
-				// fmt.Println(nodeName)
-				searchNodeMatch(nodeName, inputRegex)
+				searchNodeMatch(nodeName, inputRegex, caseSensitive)
 			}
 		} else {
 			continue
@@ -47,39 +48,57 @@ func main() {
 	}
 }
 
-func searchNodeMatch(nodeName, regex string) {
+func searchNodeMatch(nodeName, regex, caseSensitive string) {
 	filesNode, err := ioutil.ReadDir(nodeName)
 	if err != nil {
-		// log.Fatal(err)
 		log.Println(err, "error at searchNodeMatch")
+		log.Fatal(err)
 	}
 	for _, fileNode := range filesNode {
-		// fmt.Println(fileNode.Name(), fileNode.IsDir()) // -> Directories inside keg node
 		if !fileNode.IsDir() {
-			fmt.Println(nodeName, regex)
-			searchMatch(nodeName+"/README.md", regex)
+			searchMatch(nodeName+"/README.md", regex, caseSensitive)
 		}
 	}
 }
 
-func searchMatch(filepath, regex string) {
+func searchMatch(filepath, regex, caseSensitive string) {
 	file, err := os.Open(filepath)
 	if err != nil {
 		log.Println(err, "error opening fileNode")
-		// log.Fatal(err)
+		log.Fatal(err)
 	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	// optionally, resize scanner's capacity for lines over 64K, see next example
+
+	if caseSensitive != "" {
+		re, err := regexp.Compile(regex)
+		if err != nil {
+			log.Println(err, "Error with regex")
+			log.Fatal(err)
+		}
+		matchText(filepath, scanner, re)
+	} else {
+		re, err := regexp.Compile("(?i)" + regex)
+		if err != nil {
+			log.Println(err, "Error with regex")
+			log.Fatal(err)
+		}
+		matchText(filepath, scanner, re)
+	}
+
+}
+
+func matchText(filepath string, scanner *bufio.Scanner, re *regexp.Regexp) {
 	for scanner.Scan() {
-		// fmt.Println(scanner.Text())
-		re := regexp.MustCompile(regex)
-		matches := re.FindStringSubmatch(scanner.Text())
-		fmt.Println(matches[0])
+		if re.MatchString(scanner.Text()) {
+			fmt.Println(filepath)
+			fmt.Println(scanner.Text())
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
+		log.Println(err, "scanner error")
 		log.Fatal(err)
 	}
 }
